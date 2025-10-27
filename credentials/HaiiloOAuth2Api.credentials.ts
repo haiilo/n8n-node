21f4cc26b89193489d1b5fc50088230a62c5fcfd
finn.sohst <finn.sohst@haiilo.com>
2025-10-27 16:04:59 +0100

    Haiilo authentication working

--- credentials/HaiiloOAuth2Api.credentials.ts
+++ credentials/HaiiloOAuth2Api.credentials.ts
@@ -1,24 +1,44 @@
 import type { Icon, ICredentialType, INodeProperties } from 'n8n-workflow';
-import { IAuthenticate, ICredentialDataDecryptedObject, IHttpRequestOptions } from 'n8n-workflow/dist/esm/interfaces';
+import {
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	IHttpRequestOptions,
+	IHttpRequestHelper,
+	IDataObject,
+} from 'n8n-workflow';
 
 export class HaiiloOAuth2Api implements ICredentialType {
 	name = 'haiiloOAuth2Api';
 
-	extends = ['oAuth2Api'];
-
 	displayName = 'Haiilo OAuth2 API';
 
-	icon: Icon = { light: 'file:../icons/github.svg', dark: 'file:../icons/github.dark.svg' };
+	icon: Icon = { light: 'file:../icons/haiilo.svg', dark: 'file:../icons/haiilo.dark.svg' };
 
 	documentationUrl = 'https://docs.haiilo.com/restdoc/latest/general';
 
 	properties: INodeProperties[] = [
 		{
-			displayName: 'Tenant url (host)',
+			displayName: 'Tenant URL',
 			name: 'tenantUrl',
 			type: 'string',
-			default: 'https://my-tenant.haiilo.app/',
-			required: true
+			default: 'https://coyo2.eu.ngrok.io',
+			required: true,
+			description: 'Your Haiilo tenant URL (without trailing slash)',
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			default: '',
+			required: true,
+			typeOptions: { password: true },
 		},
 		{
 			displayName: 'Username',
@@ -34,23 +54,56 @@ export class HaiiloOAuth2Api implements ICredentialType {
 			typeOptions: { password: true },
 			default: '',
 			required: true,
+		},
+		{
+			displayName: 'Access Token',
+			name: 'access_token',
+			type: 'hidden',
+			default: '',
+			typeOptions: {
+				expirable: true, password: true,
+			}
 		}
 	];
-	authenticate: IAuthenticate = (
+
+	async preAuthentication(
+		this: IHttpRequestHelper,
 		credentials: ICredentialDataDecryptedObject,
-		requestOptions: IHttpRequestOptions) => {
-		// base64 encode username:password (browser-compatible
-		const bearerToken = Buffer.from(`${credentials.username}:${credentials.password}`).toString('base64');
-		if(!requestOptions.headers) requestOptions.headers = {};
-		requestOptions.headers['Authorization'] = 'Basic ' + bearerToken;
-		requestOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-		// parse tenant url and attach path and query
-		const tenantUrl = new URL(credentials.tenantUrl as string);
-		tenantUrl.pathname = '/api/oauth/token';
-		tenantUrl.searchParams.append('grant_type', 'password');
-		tenantUrl.searchParams.append('password', credentials.password as string);
-		tenantUrl.searchParams.append('username', credentials.username as string);
-		requestOptions.url = tenantUrl.toString();
-		return Promise.resolve(requestOptions);
+	): Promise<IDataObject> {
+		const tenantUrl = (credentials.tenantUrl as string).replace(/\/$/, '');
+		const basicAuth = Buffer.from(`${credentials.clientId}:${credentials.clientSecret}`).toString('base64');
+
+		const response = await this.helpers.httpRequest({
+			method: 'POST',
+			url: `${tenantUrl}/api/oauth/token`,
+			headers: {
+				'Authorization': `Basic ${basicAuth}`,
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: `grant_type=password&username=${encodeURIComponent(credentials.username as string)}&password=${encodeURIComponent(credentials.password as string)}`,
+		});
+		console.log(response);
+		return { access_token: response.access_token };
+	}
+
+	async authenticate(
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> {
+
+		if (!requestOptions.headers) {
+			requestOptions.headers = {};
+		}
+
+		requestOptions.headers['Authorization'] = `Bearer ${credentials.access_token}`;
+
+		return requestOptions;
+	}
+
+	test: ICredentialTestRequest = {
+		request: {
+			method: 'GET',
+			url: '={{$credentials.tenantUrl}}/web/users/me',
+		},
 	};
 }
