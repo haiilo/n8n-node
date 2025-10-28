27ef9d04923e37bd68923af8d90abc7f283d702e
finn.sohst <finn.sohst@haiilo.com>
2025-10-28 13:39:12 +0100

    fixed user find node

--- nodes/User/FindUser.node.ts
+++ nodes/User/FindUser.node.ts
@@ -45,50 +45,14 @@ export class FindUser implements INodeType {
 		const returnData: INodeExecutionData[] = [];
 		for (let i = 0; i < input.length; i++) {
 			const searchTerm = this.getNodeParameter('userName', 0) as string;
-			const response = await haiiloApiRequest.call(this, 'GET', '/users', {
-				q: `displayName=${searchTerm}&status=ACTIVE&with=adminFields`,
+			const response = await haiiloApiRequest.call(this, 'GET', `/users?displayName=${searchTerm}&status=ACTIVE&with=adminFields`, {
+				q: ``,
 				page: 0,
 				per_page: 10,
 			});
 
-			returnData.push({ json: response.content[0] });
+			returnData.push({ json: response.content ?? [] });
 		}
 		return [returnData];
-
-		// const items = this.getInputData();
-		//
-		// let item: INodeExecutionData;
-		// let myString: string;
-		//
-		// // Iterates over all input items and add the key "myString" with the
-		// // value the parameter "myString" resolves to.
-		// // (This could be a different value for each item in case it contains an expression)
-		// for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-		// 	try {
-		// 		myString = this.getNodeParameter('myString', itemIndex, '') as string;
-		// 		item = items[itemIndex];
-		//
-		// 		item.json.myString = myString;
-		// 	} catch (error) {
-		// 		// This node should never fail but we want to showcase how
-		// 		// to handle errors.
-		// 		if (this.continueOnFail()) {
-		// 			items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
-		// 		} else {
-		// 			// Adding `itemIndex` allows other workflows to handle this error
-		// 			if (error.context) {
-		// 				// If the error thrown already contains the context property,
-		// 				// only append the itemIndex
-		// 				error.context.itemIndex = itemIndex;
-		// 				throw error;
-		// 			}
-		// 			throw new NodeOperationError(this.getNode(), error, {
-		// 				itemIndex,
-		// 			});
-		// 		}
-		// 	}
-		// }
-		//
-		// return [items];
 	}
 }
