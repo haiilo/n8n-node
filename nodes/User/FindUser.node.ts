0caff59cdc90f8d3ff4c07909e75bf62e9d8a1e1
finn.sohst <finn.sohst@haiilo.com>
2025-10-28 11:46:11 +0100

    Find Users Node

---
+++ nodes/User/FindUser.node.ts
@@ -0,0 +1,81 @@
+import type {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+import { NodeConnectionTypes } from 'n8n-workflow';
+
+export class FindUserNode implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Find User',
+		name: 'findUser',
+		icon: { light: 'file:../../icons/haiilo.svg', dark: 'file:../../icons/haiilo.dark.svg' },
+		group: ['input'],
+		version: 1,
+		description: 'Finds a user in Haiilo',
+		defaults: {
+			name: 'Example',
+		},
+		inputs: [],
+		outputs: [NodeConnectionTypes.Main],
+		usableAsTool: true,
+		credentials: [
+			{
+				name: 'haiiloOAuth2Api',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'User Name',
+				name: 'userName',
+				type: 'string',
+				default: '',
+				placeholder: '',
+				description: 'The user name to search for',
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const searchTerm = this.getNodeParameter('userName', 0) as string;
+
+
+		// const items = this.getInputData();
+		//
+		// let item: INodeExecutionData;
+		// let myString: string;
+		//
+		// // Iterates over all input items and add the key "myString" with the
+		// // value the parameter "myString" resolves to.
+		// // (This could be a different value for each item in case it contains an expression)
+		// for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+		// 	try {
+		// 		myString = this.getNodeParameter('myString', itemIndex, '') as string;
+		// 		item = items[itemIndex];
+		//
+		// 		item.json.myString = myString;
+		// 	} catch (error) {
+		// 		// This node should never fail but we want to showcase how
+		// 		// to handle errors.
+		// 		if (this.continueOnFail()) {
+		// 			items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+		// 		} else {
+		// 			// Adding `itemIndex` allows other workflows to handle this error
+		// 			if (error.context) {
+		// 				// If the error thrown already contains the context property,
+		// 				// only append the itemIndex
+		// 				error.context.itemIndex = itemIndex;
+		// 				throw error;
+		// 			}
+		// 			throw new NodeOperationError(this.getNode(), error, {
+		// 				itemIndex,
+		// 			});
+		// 		}
+		// 	}
+		// }
+		//
+		// return [items];
+	}
+}
